deb367e968bb97adc147befead757e8bf9512cda
Volodymyr Shponarsky <ipz241_shvp@student.ztu.edu.ua>
2026-05-12 22:33:05 +0300

    Add OnTextRendered lifecycle hook

--- Program.cs
+++ Program.cs
@@ -344,6 +344,9 @@ public class LightElementNode : LightNode
         return $"<{TagName}{classAttribute}>{InnerHTML()}</{TagName}>";
     }
 
+    // Override lifecycle hooks
+    public override void OnTextRendered() { Console.WriteLine($"{TagName} text rendered"); }
+
     public override IEnumerable<LightNode> GetIterator(TraversalType type)
     {
         if (type == TraversalType.DepthFirst)
